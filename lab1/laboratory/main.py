616faaf4451c0e2e5438732b1237de9b70726a04
mnozkiewicz <michno513@gmail.com>
2025-10-12 21:12:12 +0200

    Added secrets handling

--- lab1/laboratory/main.py
+++ lab1/laboratory/main.py
@@ -1,12 +1,28 @@
 import argparse
 from dotenv import load_dotenv
 from settings import Settings
+import os
+import yaml
+
+
+def export_secrets():
+    try:
+        with open("secrets.yaml", "r") as file:
+            secrets = yaml.safe_load(file)
+
+        for key, value in secrets.items():
+            os.environ[key] = str(value)
+
+    except FileNotFoundError:
+        raise ValueError("secrets.yaml not found. No secrets loaded.")
 
 
 def export_envs(environment: str = "dev") -> None:
     Settings.validate_environment(environment)
     env_file = f".envs/.env.{environment}"
     load_dotenv(env_file)
+
+    export_secrets()
 
 
 if __name__ == "__main__":
@@ -27,3 +43,4 @@ if __name__ == "__main__":
 
     print("APP_NAME: ", settings.APP_NAME)
     print("ENVIRONMENT: ", settings.ENVIRONMENT)
+    print("API_KEY: ", settings.API_KEY)
