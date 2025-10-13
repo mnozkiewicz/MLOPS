3b1f0186713c2c18dbcdd3f41d66b270f582d34b
mnozkiewicz <michno513@gmail.com>
2025-10-13 20:33:30 +0200

    Added docstrings to lab task

--- lab1/laboratory/src/model/inference.py
+++ lab1/laboratory/src/model/inference.py
@@ -6,6 +6,9 @@ from pathlib import Path
 
 
 def load_model() -> LogisticRegression:
+    """
+    Load a trained LogisticRegression model from the saved_models folder.
+    """
     current_dir = Path(__file__).parent.resolve()
     model_path = f"{current_dir}/saved_models/model.joblib"
     model: LogisticRegression = joblib.load(model_path)
@@ -16,6 +19,9 @@ def load_model() -> LogisticRegression:
 def predict(
     model: LogisticRegression, X: Union[pd.DataFrame, list[list[float]]]
 ) -> List[str]:
+    """
+    Predict labels for the given input data using the provided model.
+    """
     if not isinstance(X, pd.DataFrame):
         X = pd.DataFrame(X)
 
--- lab1/laboratory/src/model/training.py
+++ lab1/laboratory/src/model/training.py
@@ -7,18 +7,27 @@ from pathlib import Path
 
 
 def load_data() -> Tuple[pd.DataFrame, pd.Series]:
+    """
+    Load the Iris dataset and return features and target.
+    """
     df, y = load_iris(return_X_y=True, as_frame=True)
     df.columns = ["sepal_length", "sepal_width", "petal_length", "petal_width"]
     return df, y
 
 
 def train_model(df: pd.DataFrame, y: pd.Series) -> LogisticRegression:
+    """
+    Train a logistic regression model on the given data.
+    """
     model = LogisticRegression(max_iter=100)
     model.fit(df, y)
     return model
 
 
 def save_model(model: LogisticRegression) -> None:
+    """
+    Save the trained model to the `saved_models` folder.
+    """
     current_dir = Path(__file__).parent.resolve()
     saving_path = f"{current_dir}/saved_models/model.joblib"
     joblib.dump(model, saving_path)
